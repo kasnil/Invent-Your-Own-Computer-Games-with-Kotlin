1bb165acda5612362b302b417eeb38def0ab3bb8
kasnil <kasnil@outlook.ie>
2025-07-04 13:57:58 +0300

    Jokes

---
+++ jokes/Main.kt
@@ -0,0 +1,20 @@
+package com.github.kasnil.kotlin.invent.jokes
+
+fun main() {
+    println("What do you get when you cross a snowman with a vampire?")
+    readLine()
+    println("Frostbite!")
+    println()
+    println("What do dentists call an astronaut's cavity?")
+    readLine()
+    println("A black hole!")
+    println()
+    println("Knock knock.")
+    readLine()
+    println("Who's there?")
+    readLine()
+    println("Interrupting cow.")
+    readLine()
+    print("Interrupting cow wh")
+    println("-MOO!")
+}
